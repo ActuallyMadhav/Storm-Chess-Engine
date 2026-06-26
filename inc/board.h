bdc6d8cef171922de6b031b8974a89ff5ab46118
ActuallyMadhav <madhavra.2004@gmail.com>
2026-06-27 01:15:51 +0800

    journey of a thousand miles begins with a single step ahh commit

---
+++ inc/board.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+// macros
+#define unsigned long long u64
+
+enum pieces {
+    empty = 0,  // empty square
+
+    // white pieces
+    wpawn = 1, wknight = 2, wbishop = 3, wrook = 4, wqueen = 5, wking = 6,
+    
+    // black pieces
+    bpawn = 7, bknight = 8, bbishop = 9, brook = 10, bqueen = 11, bking = 12
+};
+
+class Board{
+private:
+
+public:
+    
+};
